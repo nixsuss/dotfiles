554b3d4da243e83eeed8e8792ec01d0421a744c8
nixsuss <anixsuss@protonmail.com>
2022-11-12 22:24:36 +0200

    libreoffice script

---
+++ Void/libreoffice-void.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -ue
+sudo xbps-install -S libreoffice-gnome libreoffice-i18n-ar libreoffice-i18n-en-US libreoffice-writer libreoffice-calc libreoffice-impress openjdk libreoffice-fonts
