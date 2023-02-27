ac7bfcf366b26e0d78615c54cc8c0fdb691d6338
nixsuss <anixsuss@protonmail.com>
2023-02-27 10:14:44 +0200

    lutris script

--- Void/void-lutris.sh
+++ Void/void-lutris.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
-sudo xbps-install -Sy MangoHud MangoHud-32bit gamemode wine wine-32bit lutris
+sudo xbps-install -Sy MangoHud MangoHud-32bit gamemode wine wine-32bit lutris libgamemode-32bit
 sudo makewhatis -a
