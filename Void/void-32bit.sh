e24cd8c0f3732e3029dc6128e8c98fa8d7eb0897
nixsuss <anixsuss@protonmail.com>
2023-05-02 17:24:02 +0300

    void 32

--- Void/void-32bit.sh
+++ Void/void-32bit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 sudo xbps-install -Sy void-repo-multilib-nonfree void-repo-multilib
-sudo xbps-install -Sy nvidia-libs-32bit vulkan-loader-32bit
+sudo xbps-install -Sy nvidia-libs-32bit vulkan-loader-32bit libdrm-32bit
 sudo ldconfig
