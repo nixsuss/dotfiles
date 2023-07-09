1056725b1f3332d1bcc9eabfd4338cb2abd8a4de
nixsuss <anixsuss@protonmail.com>
2023-07-09 18:51:08 +0300

    lf fix

--- DWM/dwm-9-April-notitle/config.h
+++ DWM/dwm-9-April-notitle/config.h
@@ -80,6 +80,7 @@ static const Rule rules[] = {
 	{ "obs",     NULL,     NULL,           0,         1,          -1 },
 	{ "Audacious",     NULL,     NULL,           0,         1,     -1 },
 	{ "Nsxiv",     NULL,     NULL,           0,         1,     -1 },
+	{ "Zathura",     NULL,     NULL,           0,         1,     -1 },	
 	{ NULL,      NULL,     "Event Tester", 0,         0,           -1 }, /* xev */
 	{ "Firefox", "Toolkit", "Picture-in-Picture", 0,          1,   -1 },
 	{ "firefox", "Toolkit", "Picture-in-Picture", 0,          1,   -1 },
