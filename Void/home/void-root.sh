947b6e2aca8c3fd2af53ce78c5ecf1fabcf05de3
nixsuss <anixsuss@protonmail.com>
2025-05-20 15:57:34 +0300

    commit

--- Void/home/void-root.sh
+++ Void/home/void-root.sh
@@ -2,6 +2,6 @@
 xbps-install -Su
 xbps-install -Suy xbps
 xbps-install -Sy void-repo-nonfree
-xbps-install -Sy intel-ucode
+xbps-install -Sy intel-ucode linux-firmware
 xbps-reconfigure -afv
 chsh -s /bin/bash
