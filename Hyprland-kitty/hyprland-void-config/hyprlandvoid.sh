4ac3c4e5435280c9b421048899b1c40b3d839f32
nixsuss <anixsuss@protonmail.com>
2025-05-21 13:56:18 +0300

    remove mitigation=off

--- Hyprland-kitty/hyprland-void-config/hyprlandvoid.sh
+++ Hyprland-kitty/hyprland-void-config/hyprlandvoid.sh
@@ -2,7 +2,7 @@
 set -eu
 echo repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc | sudo tee /etc/xbps.d/hyprland-void.conf
 sudo xbps-install -Suy wofi seatd hypridle grim mako hyprpaper hyprlock jq wl-clipboard cliphist foot mpDris2 pqiv hyprland xdg-desktop-portal-hyprland hyprland-qtutils Waybar xorg-server-xwayland swww
-sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 mitigations=off/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 mitigations=off nvidia_drm.modeset=1 nvidia_drm.fbdev=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1/' /etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 nvidia_drm.modeset=1 nvidia_drm.fbdev=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo usermod -aG _seatd $USER
 sudo ln -sv /etc/sv/seatd /var/service/
