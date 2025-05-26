caafb2a80395f4968a39fa10dfc6adb9fb9db336
nixsuss <anixsuss@protonmail.com>
2025-05-26 17:48:14 +0300

    committt

--- Arch/home/archlinux-install.sh
+++ Arch/home/archlinux-install.sh
@@ -4,7 +4,7 @@ sudo timedatectl set-ntp true
 sudo pacman -Syyu --noconfirm
 sudo pacman -S --noconfirm archlinux-keyring
 sudo pacman -S --needed xorg-server xorg-xrdb xorg-xrandr xorg-xprop xorg-setxkbmap xorg-xinit xorg-server-devel nvidia nvidia-utils pipewire pipewire-pulse pipewire-alsa alsa-utils wireplumber pipewire-jack ttf-dejavu ttf-cascadia-code ttf-fira-code noto-fonts-extra noto-fonts-emoji ttf-hack gvim xdg-utils scrot rxvt-unicode wget git slock nitrogen picom zathura mpv gparted gnome-disk-utility pcmanfm engrampa leafpad dconf-editor galculator papirus-icon-theme ntfs-3g mtpfs gvfs-mtp gvfs-gphoto2 gvfs rsync htop mpc qbittorrent ncmpcpp zsh zsh-completions mpd qt5ct qt6ct kvantum ufw nwg-look base-devel pavucontrol breeze-icons capitaine-cursors polkit-gnome smartmontools xss-lock rofi torbrowser-launcher torsocks numlockx lsd redshift nsxiv xclip figlet lolcat libx11 libxinerama libxft capitaine-cursors curl dialog mtools ttf-scheherazade-new bottom dust yt-dlp bat ffmpeg conky fribidi kitty lf trash-cli terminus-font ffnvcodec-headers python-pywal xdotool sound-theme-freedesktop zsh-autosuggestions zsh-syntax-highlighting dunst ttf-jetbrains-mono-nerd fastfetch
-sudo pacman -S --aussme-installed dmenu clipmenu --noconfirm
+sudo pacman -S --assume-installed dmenu clipmenu --noconfirm
 sudo systemctl enable fstrim.timer
 sudo systemctl enable ufw 
 sudo ufw enable
