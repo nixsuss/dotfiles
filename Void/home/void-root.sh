3da4232395176975ebb1eeb7f50e8e39b0ec43da
nixsuss <anixsuss@protonmail.com>
2025-05-30 18:43:53 +0000

    change

--- Void/home/void-root.sh
+++ Void/home/void-root.sh
@@ -2,6 +2,6 @@
 xbps-install -Su
 xbps-install -Suy xbps
 xbps-install -Sy void-repo-nonfree
-xbps-install -Sy intel-ucode linux-firmware
+xbps-install -Sy intel-ucode linux-firmware nano sof-firmware
 xbps-reconfigure -afv
 chsh -s /bin/bash
