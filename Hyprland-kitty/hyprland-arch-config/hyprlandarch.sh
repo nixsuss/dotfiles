a04ea3d2475b195cf0769c370849e6fe8bfb3ed4
nixsuss <anixsuss@protonmail.com>
2025-05-30 11:50:14 +0300

    commit

--- Hyprland-kitty/hyprland-arch-config/hyprlandarch.sh
+++ Hyprland-kitty/hyprland-arch-config/hyprlandarch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-sudo pacman -S --needed --noconfirm wofi hypridle grim mako hyprpaper hyprlock jq wl-clipboard cliphist foot mpd-mpris pqiv hyprland xdg-desktop-portal-hyprland hyprland-qtutils waybar swww hyprland-protocols python-pywayland
+sudo pacman -S --needed --noconfirm wofi hypridle grim mako hyprpaper hyprlock jq wl-clipboard cliphist foot mpd-mpris hyprland xdg-desktop-portal-hyprland hyprland-qtutils waybar swww hyprland-protocols python-pywayland
 sudo sed -i 's|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1 nvidia_drm.fbdev=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1"|' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 cp -rv config/* ~/.config/
--- Hyprland-kitty/hyprland-void-config/hyprlandvoid.sh
+++ Hyprland-kitty/hyprland-void-config/hyprlandvoid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 echo repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc | sudo tee /etc/xbps.d/hyprland-void.conf
-sudo xbps-install -Suy wofi seatd hypridle grim mako hyprpaper hyprlock jq wl-clipboard cliphist foot mpDris2 pqiv hyprland xdg-desktop-portal-hyprland hyprland-qtutils Waybar xorg-server-xwayland swww
+sudo xbps-install -Suy wofi seatd hypridle grim mako hyprpaper hyprlock jq wl-clipboard cliphist foot mpDris2 hyprland xdg-desktop-portal-hyprland hyprland-qtutils Waybar xorg-server-xwayland swww
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 nvidia_drm.modeset=1 nvidia_drm.fbdev=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo usermod -aG _seatd $USER
