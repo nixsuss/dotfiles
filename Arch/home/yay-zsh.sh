e5c8170a6c40f4fe41b452d2e01b9ccc7d63afe6
nixsuss <anixsuss@protonmail.com>
2025-06-01 00:30:25 +0300

    fonts

--- Arch/home/yay-zsh.sh
+++ Arch/home/yay-zsh.sh
@@ -5,4 +5,5 @@ cd yay
 makepkg -si
 cd
 yay -Syyu
-yay -S ttf-amiri ttf-sil-lateef ttf-arabeyes-fonts librewolf-bin
+yay -S ttf-amiri ttf-sil-lateef ttf-arabeyes-fonts librewolf-bin ttf-cairo ttf-ubuntu-arabic
+fc-cache -rv
