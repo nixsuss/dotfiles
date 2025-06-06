b85d69e2f70d8ac5eb3e1368461cfd0527ce2601
nixsuss <anixsuss@protonmail.com>
2025-06-06 17:53:12 +0300

    com

--- Void/void-gaming.sh
+++ Void/void-gaming.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-sudo xbps-install -Sy gamemode wine wine-32bit libgamemode-32bit winetricks wine-mono freetype-32bit gamescope gamescope-32bit gst-plugins-bad1 gst-libav gst-libav-32bit gst-plugins-bad1-32bit gst-plugins-ugly1 gst-plugins-ugly1-32bit gst-plugins-good1-32bit gst-plugins-good1 gst-plugins-base1 gst-plugins-base1-32bit qarma 7zip gstreamer1 gstreamer1-32bit gstreamer1-pipewire gstreamer1-pipewire-32bit
+sudo xbps-install -Sy gamemode wine wine-32bit libgamemode-32bit winetricks wine-mono freetype-32bit gamescope gamescope-32bit gst-plugins-bad1 gst-libav gst-libav-32bit gst-plugins-bad1-32bit gst-plugins-ugly1 gst-plugins-ugly1-32bit gst-plugins-good1-32bit gst-plugins-good1 gst-plugins-base1 gst-plugins-base1-32bit qarma gstreamer1 gstreamer1-32bit gstreamer1-pipewire gstreamer1-pipewire-32bit
 sudo makewhatis -a
 echo
 echo "Mangohud is not installed!!!"
