f03ea442f5d96e52ab93401d92f5b9c04a2ca63d
nixsuss <anixsuss@protonmail.com>
2025-06-07 00:06:42 +0300

    commit

--- Arch/arch-gaming.sh
+++ Arch/arch-gaming.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
 sudo sed -i '/^\#\[multilib\]/s/^#//' /etc/pacman.conf
-sudo sed -i '/^\#Include = \/etc\/pacman.d\/mirrorlist/s/^#//' /etc/pacman.conf
+sudo sed -i '/^\[multilib\]/ {n; s/^#//}' /etc/pacman.conf
 sudo pacman -Syy
 sudo pacman -S --needed --noconfirm gamemode lib32-gamemode wine-staging winetricks wine-mono lib32-freetype2 gamescope gst-plugins-bad gst-plugins-good gst-plugins-ugly gst-plugins-base lib32-gst-plugins-base lib32-gst-plugins-good gst-libav lib32-mangohud mangohud lib32-nvidia-utils gstreamer zenity goverlay lib32-gstreamer
